27075aada2c5d3aa6262eeb456ce74ba68e298e6
adithyan <adithyancl99@gmail.com>
2026-01-09 16:06:26 +0530

    settings.py updated

--- backend/medic/settings.py
+++ backend/medic/settings.py
@@ -37,7 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'doctors',
+    'users',
+    'appointments',
+    'rest_framework',
+    'rest_framework_simplejwt',
+] 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
